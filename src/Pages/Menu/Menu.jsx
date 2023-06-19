6db6859e231924e6108928481366e47e2b5708bc
Md Rifat Hossen <rifatsaown0@gmail.com>
2023-06-19 23:21:18 +0600

    Paralax Added

--- src/Pages/Menu/Menu.jsx
+++ src/Pages/Menu/Menu.jsx
@@ -1,4 +1,6 @@
 import { Helmet } from "react-helmet-async";
+import Cover from "../Shared/Cover";
+import img from "../../assets/menu/banner3.jpg";
 
 const Menu = () => {
   return (
@@ -6,7 +8,7 @@ const Menu = () => {
       <Helmet>
         <title>Bistro Boss | Menu</title>
       </Helmet>
-      <p>Hello</p>
+      <Cover img={img} title='our menu'/>
     </div>
   );
 };
